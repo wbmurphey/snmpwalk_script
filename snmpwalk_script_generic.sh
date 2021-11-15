0cf1a9db693441ac444c724304a3d6b0b3a15278
wbmurphey <85957424+wbmurphey@users.noreply.github.com>
2021-11-15 13:54:35 -0700

    changed output to generic name

--- snmpwalk_script_generic.sh
+++ snmpwalk_script_generic.sh
@@ -7,10 +7,10 @@ now=$(date '+%F_%H:%M')
 while read ip; do
   if snmpwalk -Oqv -t 1 -r 1 -v 2c -c communitystring ${ip} sysName.0;
     then hostname=$(snmpwalk -Oqv -t 1 -r 1 -v 2c -c communitystring ${ip} sysName.0);
-    printf "${ip}, ${hostname}\n" >> /home/wmurphey/verified_devices/libre_verified_${1}_${now}.csv;
+    printf "${ip}, ${hostname}\n" >> /home/wmurphey/verified_devices/verified_${1}_${now}.csv;
   elif snmpwalk -Oqv -t 1 -r 1 -v 2c -c communitystring2 ${ip} sysName.0;
     then hostname=$(snmpwalk -Oqv -t 1 -r 1 -v 2c -c communitystring2 ${ip} sysName.0);
-    printf "${ip}, ${hostname}\n" >> /home/wmurphey/verified_devices/libre_verified_${1}_${now}.csv;
-  else echo "${ip}" >> /home/wmurphey/unverified_devices/libre_unverified_${1}_${now}.csv;
+    printf "${ip}, ${hostname}\n" >> /home/wmurphey/verified_devices/verified_${1}_${now}.csv;
+  else echo "${ip}" >> /home/wmurphey/unverified_devices/unverified_${1}_${now}.csv;
   fi
 done <$1
